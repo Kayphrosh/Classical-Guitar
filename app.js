2dd417dcd9447c0510da22a85758f9fd56e445d1
kayphrosh <olakunbiolabode@yahoo.com>
2022-07-03 15:06:49 +0100

    done!

--- app.js
+++ app.js
@@ -5,3 +5,28 @@ readmoreToggle.forEach(toggle => {
     toggle.parentNode.classList.toggle ('read-active')
   })
 })
+
+
+function onYouTubeIframeAPIReady() {
+  var player;
+  player = new YT.Player('youtube-video', {
+    videoId: 'wsALfL4BYAE',
+    playerVars: {
+      autoplay: 1,        
+      controls: 0,    
+      showinfo: 1,
+      modestbranding: 0, 
+      loop: 1,  
+      fs: 0,
+      cc_load_policy: 0,
+      iv_load_policy: 3, 
+      autohide: 1,  
+      mute:1
+    },
+    events: {
+      onReady: function(e) {
+        e.target.mute();
+      }
+    }
+  });
+ }
